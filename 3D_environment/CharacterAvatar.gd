4672250583dfd0faea783baad2bf2673d000b81e
JayAiJoe <jayailocsin@gmail.com>
2022-09-12 23:19:13 +0800

    Ring Add/Remove Character

--- 3D_environment/CharacterAvatar.gd
+++ 3D_environment/CharacterAvatar.gd
@@ -24,18 +24,23 @@ func set_data(id : int) -> void:
 	#TODO : connect to character database
 	$Sprite3D.texture = load("res://assets/samplechar/char" + str(id) + ".png")
 
-func go_to_angle(new_angle : float, new_radius : float = radius) -> void:
-	if not tween_locked:
+# animate movement towards new angle
+# lock_override is for high priority calls such as spawning
+func go_to_angle(new_angle : float, time : float = 0.3, lock_override : bool = false, new_radius : float = radius) -> void:
+	if not tween_locked or lock_override:
 		var tween = create_tween().set_ease(Tween.EASE_IN_OUT)
 		var temp = curr_deg
 		tween_locked = true
-		tween.tween_method(self, "move_along_circle", temp, new_angle, 0.3)
-		tween.parallel().tween_property(self, "radius", new_radius, 0.3)
+		if new_angle - temp > 180.0:
+			new_angle -= 360
+		tween.tween_method(self, "move_along_circle", temp, new_angle, time)
+		tween.parallel().tween_property(self, "radius", new_radius, time)
 		yield(tween, "finished")
 		tween_locked = false
 
+
 func move_along_circle(new_deg : float) -> void:
-	curr_deg= fmod(new_deg, 360)
+	curr_deg = fmod(new_deg, 360)
 	var x = cos(deg2rad(curr_deg) ) * radius
-	var y = sin(deg2rad(curr_deg) ) * radius
-	translation = Vector3(x,0,y)
+	var z = sin(deg2rad(curr_deg) ) * radius
+	translation = Vector3(x,0,z)
--- 3D_environment/Ring3D.gd
+++ 3D_environment/Ring3D.gd
@@ -3,16 +3,14 @@ class_name Ring3D
 
 export var RingAvatar = preload("res://3D_environment/CharacterAvatar.tscn")
 
-var characters = []
+var characters := []
+var door : CharacterAvatar
+var current_character : CharacterAvatar
 
 func _ready() -> void:
-	for i in range(4):
-		var ca = RingAvatar.instance()
-		ca.set_data(i + 1)
-		characters.append(ca)
-		add_child(ca)
-		ca.move_along_circle(i * 90.0)
-
+	randomize()
+	initialize_door()
+	
 func _input(event: InputEvent) -> void:
 	if event is InputEventMouseButton:
 		if event.is_pressed():
@@ -20,17 +18,65 @@ func _input(event: InputEvent) -> void:
 				rotate_ring(1)
 			elif event.button_index == BUTTON_WHEEL_DOWN:
 				rotate_ring(-1)
-		
+	elif event.is_action_pressed("ui_accept"):
+		add_new_character(randi() % 2 + 2)
+	elif event.is_action_pressed("ui_down"):
+		remove_character(characters[-1])
+
+func initialize_door() -> void:
+	door = RingAvatar.instance()
+	door.set_data(1)
+	characters.append(door)
+	add_child(door)
+	door.move_along_circle(90)
+	current_character = door
 
 # rotates the ring 1 unit in the specified direction
 # 1 clockwise, -1 counterclockwise
 func rotate_ring(direction : int) -> void:
 	for c in characters:
-		c.go_to_angle(c.get_current_angle() + direction * 90)
+		c.go_to_angle(c.get_current_angle() + direction * get_rotate_angle(), 0.1)
+	var index = characters.find(current_character)
+	index = (index + direction) % characters.size() # TODO: possible direction bug for current character focusing
+	current_character = characters[index]
 
-		
+# adds a new character to the ring
+func add_new_character(cid : int) -> void:
+	var ca = RingAvatar.instance()
+	ca.set_data(cid)
+	characters.append(ca)
+	add_child(ca)
+	ca.move_along_circle(door.get_current_angle())
+	adjust_ring_positions()
 
-func get_angles(count : int, offset : float = 0.0) -> Array:
+# removes the specified character from the ring
+# cannot remove the door
+func remove_character(ca : CharacterAvatar) -> void:
+	if ca != door:
+		characters.erase(ca)
+		ca.queue_free()
+		adjust_ring_positions()
+
+
+# corrects the positions of characters currently in the ring
+func adjust_ring_positions() -> void:
+	var temp_door = door.get_current_angle()
+	
+	var new_positions = get_ring_positions(characters.size(), temp_door) # new positions with door as anchor
+	for c_index in range(characters.size()): 
+		characters[c_index].go_to_angle(new_positions[c_index], 0.3, true)
+	
+	
+	
+	
+	
+
+# returns the angle for 1 rotation
+func get_rotate_angle() -> float:
+	return 360.0 / characters.size()
+
+# returns array of angular positions along the ring based on the number of characters
+func get_ring_positions(count : int, offset : float = 0.0) -> Array:
 	var spacing = 360.0 / count
 	var res = []
 	for i in count:
